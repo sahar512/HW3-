f0d07c8e1b0389561194c0695af648310eba9739
sahar512 <118927236+sahar512@users.noreply.github.com>
2023-01-10 00:52:27 +0200

    Update isort.c

--- isort.c
+++ isort.c
@@ -1,71 +1,45 @@
 #include <stdio.h>
-#define fifty 50
+#define FIFTY 50
 void shift_element(int *arr,int i)
 {
-    int temp=i; 
-    while (temp>0){
-        *(arr+temp)=*(arr+temp-1);
-        temp--;
+    int temp=i;
+    while (temp>0)
+    {
+    *(arr+temp)=*(arr+temp-1);
+    temp--;
     }
 }
 void insertion_sort(int *arr,int len)
 {
-    for(int i=1; i<len; i++){
-        int temp1=*(arr+i);
-        int j =i-1;
-        while(j>=0){
-            if((*(arr+j)>temp1)){
-                j--; 
-            }
-            else{
-                break;
-            }
+    int temp1,temp2,i=1;
+    for ( i = 1; i < len; i++)
+    {
+        temp1=*(arr+i);
+        temp2=i-1;
+        while(temp2>=0 && *(arr+temp2)>temp1)
+        {
+         *(arr+temp2+1)=*(arr+temp2);   
+         temp2--;
         }
-        shift_element(arr+j, i-j);
-        *(arr+j+1)=temp1; 
-        // while(j>=0 && temp1 < *(arr+j)){
-        //     j--;
-        //     shift_element((arr+i),(j-i));
-        //     *(arr+i+1)=temp1;
-        // }
-    }
-    // int i=1;
-    // while (i<=len)
-    // {
-    //     int temp1=*(arr+i);
-    //     int j=i-1;
-    //     int temp2=*(arr+j);
-    //     // while(temp1<temp2 && j>=0)
-    //     //   j--;
-    //     // shift_element(arr+j,i-j);
-    //     // *(arr+j+1)=temp1;  
-    // } 
+        *(arr+temp2+1)=temp1;  
+    } 
 }
 int main()
 {
-    int arr[fifty]={0};
-    for (int i = 0; i < fifty; i++)
+    int arr[FIFTY]={0};
+    for (int i = 0; i < FIFTY; i++)
     {
         scanf("%d",&(*(arr+i)));
     }
-    insertion_sort(arr,fifty);
-    for(int i=0; i<fifty;i++)
+    insertion_sort(arr,FIFTY);
+    int i = 0; 
+    for ( i = 0; i < FIFTY; ++i)
     {
-        if(i==fifty-1){
-            printf("%d",*(arr+i));
-            break;
+        if (i!=FIFTY-1)
+        {
+            printf("%d%c",*(arr+i),',');
         }
-        else{
-            printf(",");
-        }
-        printf("%d",*(arr+i));
+        else 
+        printf("%d%c",*(arr+i),'\n');
     }
-    printf("\n");
-    // int i = 0; 
-    // while(i < fifty)
-    // {
-    //     printf("%d ",(*(arr+i)));
-    //     i++;
-    // }
-    // return 0;
 }
